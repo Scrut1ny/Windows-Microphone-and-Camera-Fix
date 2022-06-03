5e827e69178986ca8bf489456c3d27caaea4f2bb
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2022-06-03 18:55:23 +0000

    Update Microphone+Webcam Fix.bat

--- Microphone+Webcam Fix.bat
+++ Microphone+Webcam Fix.bat
@@ -11,12 +11,9 @@ fltmc >nul 2>&1 || (
 )
 
 :MENU
-cls
-echo(
-echo   [31m1[0m - [32mAllow[0m Microphone ^& Camera Access
+cls&echo(&echo   [31m1[0m - [32mAllow[0m Microphone ^& Camera Access
 echo   [31m2[0m - [31mDeny[0m Microphone ^& Camera Access
-echo   [31m3[0m - Exit
-echo(
+echo   [31m3[0m - Exit&echo(
 set /p c=".  [35m#[0m "
 if %c%==1 goto :ALLOW
 if %c%==2 goto :DENY
@@ -27,7 +24,6 @@ exit /b
 :ALLOW
 >nul 2>&1(
 	rem Camera ==============================================================================================================================================
-	
 	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam" /v "Value" /d "Allow" /t REG_SZ /f
 	rem For older Windows (before 1903)
 	reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{E5323777-F976-4f5b-9B55-B94699C46E44}" /t REG_SZ /v "Value" /d "Allow" /f
@@ -36,9 +32,7 @@ exit /b
 	reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCamera_ForceAllowTheseApps" /f
 	reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCamera_ForceDenyTheseApps" /f
 	reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCamera_UserInControlOfTheseApps" /f
-	
 	rem Microphone ==========================================================================================================================================
-	
 	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Allow" /f
 	rem For older Windows (before 1903)
 	reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{2EEF81BE-33FA-4800-9670-1CD474972C3F}" /v "Value" /t REG_SZ /d "Allow" /f
@@ -49,14 +43,12 @@ exit /b
 	reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessMicrophone_UserInControlOfTheseApps" /f
 	gpupdate /force
 )
-cls&echo(&echo   All camera ^& microphone access [32mallowed[0m.&>nul pause
-goto :MENU
+cls&echo(&echo   All camera ^& microphone access [32mallowed[0m.&>nul pause&goto :MENU
 exit /b
 
 :DENY
 >nul 2>&1(
 	rem Camera ==============================================================================================================================================
-	
 	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam" /v "Value" /d "Deny" /t REG_SZ /f
 	rem For older Windows (before 1903)
 	reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{E5323777-F976-4f5b-9B55-B94699C46E44}" /t REG_SZ /v "Value" /d "Deny" /f
@@ -65,9 +57,7 @@ exit /b
 	reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCamera_ForceAllowTheseApps" /t REG_MULTI_SZ /f
 	reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCamera_ForceDenyTheseApps" /t REG_MULTI_SZ /f
 	reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCamera_UserInControlOfTheseApps" /t REG_MULTI_SZ /f
-	
 	rem Microphone ==========================================================================================================================================
-	
 	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Deny" /f
 	rem For older Windows (before 1903)
 	reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{2EEF81BE-33FA-4800-9670-1CD474972C3F}" /v "Value" /t REG_SZ /d "Deny" /f
@@ -78,6 +68,5 @@ exit /b
 	reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessMicrophone_UserInControlOfTheseApps" /t REG_MULTI_SZ /f
 	gpupdate /force
 )
-cls&echo(&echo   All camera ^& microphone access [31mdenied[0m.&>nul pause
-goto :MENU
+cls&echo(&echo   All camera ^& microphone access [31mdenied[0m.&>nul pause&goto :MENU
 exit /b 0
